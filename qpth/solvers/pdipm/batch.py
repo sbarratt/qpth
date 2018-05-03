89b9bef506cd25e130cddf7a7b5c12dc7494e240
Shane Barratt <sbarratt@berkeley.edu>
2018-05-03 14:19:20 -0700

    fixed shape bug

--- qpth/solvers/pdipm/batch.py
+++ qpth/solvers/pdipm/batch.py
@@ -82,12 +82,12 @@ def forward(Q, p, G, h, A, b, Q_LU, S_LU, R, eps=1e-12, verbose=0, notImprovedLi
     # Make all of the slack variables >= 1.
     M = torch.min(s, 1)[0].repeat(1, nineq)
     I = M < 0
-    s[I] -= M[I] - 1
+    s[I.view(s.size())] -= M[I] - 1
 
     # Make all of the inequality dual variables >= 1.
     M = torch.min(z, 1)[0].repeat(1, nineq)
     I = M < 0
-    z[I] -= M[I] - 1
+    z[I.view(s.size())] -= M[I] - 1
 
     best = {'resids': None, 'x': None, 'z': None, 's': None, 'y': None}
     nNotImproved = 0
